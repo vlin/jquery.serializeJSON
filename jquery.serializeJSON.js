50bcf1467ae3aadd1e79897fe18b3808d18b5cfc
vlin <vip.ljf@gmail.com>
2014-01-11 17:07:26 +0800

    rewrite the code
    return a array if element is a checkbox;

--- jquery.serializeJSON.js
+++ jquery.serializeJSON.js
@@ -2,21 +2,22 @@
   'use strict';
   $.fn.serializeJSON = function() {
     var json = {};
-    $.each(this.serializeArray(), function() {
-      var key = this.name,
-        val = this.value;
-      if (val !== '' && val !== undefined) {
-        if (key in json) {
-          if ($.isArray(json[key])) {
-            json[key].push(val);
-          } else {
-            json[key] = [json[key], val];
-          }
+    $.each(this.find('input,select,textarea'), function(i) {
+      var el = $(this),
+        key = el.attr('name'),
+        val = el.val();
+
+      if (val != '' && val !== undefined && val !== null) {
+        if (el.is(':checkbox')) {
+          el.prop('checked') && ($.isArray(json[key]) ? json[key].push(val) : json[key] = [val]);
+        } else if (el.is(':radio')) {
+          el.prop('checked') && (json[key] = val);
         } else {
           json[key] = val;
         }
       }
     });
+
     return json;
   };
 })(jQuery);
